eae3f11f29e6df2c777adb7a8272afdf0036882a
Ayush Kumar Singh <shkmrsingh@gmail.com>
2022-11-30 10:18:16 +0530

    Done

--- login.js
+++ login.js
@@ -1,2 +1,15 @@
 let login_id= "sudip@gmail.com";
 let login_pass = "123@su";
+let num=13 
+let r=0
+    for(i=1;i<=num;i++){
+        if(num%i==0){
+            r+=1;
+        }
+     }
+        if(r==2){
+         console.log("Yes")
+     }   
+     else{
+         console.log("No")
+     }
